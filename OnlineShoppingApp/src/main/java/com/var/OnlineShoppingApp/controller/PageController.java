1ba0b60f54aeb07b4b88055d956d2c92274279c5
689807 <689807@PC219702.cts.com>
2018-04-13 17:01:42 +0530

    I deleted CONTROLLER LOGIC

--- OnlineShoppingApp/src/main/java/com/var/OnlineShoppingApp/controller/PageController.java
+++ OnlineShoppingApp/src/main/java/com/var/OnlineShoppingApp/controller/PageController.java
@@ -1,35 +1,6 @@
 package com.var.OnlineShoppingApp.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-@Controller
 public class PageController {
-	@RequestMapping(value= {"/","/index","/home"})
-	public ModelAndView index()
-	{
-		//i got it
-		ModelAndView mv=new ModelAndView("page");
-		mv .addObject("title","home");
-		mv.addObject("userClicksHome",true);
-		return mv;
-	}
-	@RequestMapping(value= "/about")
-	public ModelAndView about()
-	{
-		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("title","About us");
-		mv.addObject("userClicksAbout",true);
-		return mv;
-	}
-	@RequestMapping(value= "/contact")
-	public ModelAndView contact()
-	{
-		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("title","Contact us");
-		mv.addObject("userClicksContact",true);
-		return mv;
-	}
-
+	
 }
