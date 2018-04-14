85b8310438ca21d202f10f305336d7ee0f6f66e7
varma9160810725 <35567317+varma9160810725@users.noreply.github.com>
2018-04-14 20:15:58 +0530

    Update PageController.java

--- OnlineShoppingApp/src/main/java/com/var/OnlineShoppingApp/controller/PageController.java
+++ OnlineShoppingApp/src/main/java/com/var/OnlineShoppingApp/controller/PageController.java
@@ -4,8 +4,7 @@ package com.var.OnlineShoppingApp.controller;
 public class PageController {
 	public static void main(String[] args) {
 		
-		System.out.println("varma");
-		System.out.println("varma");
+		
 
 		
 	}
