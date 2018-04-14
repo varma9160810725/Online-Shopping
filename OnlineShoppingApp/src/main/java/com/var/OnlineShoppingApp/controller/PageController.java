d9b303fc582eb3a71fada0134c843ab257f64794
LokeshChowdary <lokeshchowdary.kommineni@gmail.com>
2018-04-14 13:44:06 +0530

    removed some lines.

--- OnlineShoppingApp/src/main/java/com/var/OnlineShoppingApp/controller/PageController.java
+++ OnlineShoppingApp/src/main/java/com/var/OnlineShoppingApp/controller/PageController.java
@@ -3,14 +3,10 @@ package com.var.OnlineShoppingApp.controller;
 
 public class PageController {
 	public static void main(String[] args) {
-		System.out.println("hai");
+		
 		System.out.println("varma");
 		System.out.println("varma");
 		System.out.println("varma");
-		System.out.println("varma");
-		System.out.println("varma");
-		System.out.println("varma");
-		System.out.println("varma");
-		System.out.println("lokesh");
+		
 	}
 }
