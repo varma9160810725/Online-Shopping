9eb0bc51f44bc5d4f66de1dfb733b4d4cc5492a9
varma <varma.mandati@gmail.com>
2018-04-14 22:51:35 +0530

    adding

--- OnlineShoppingApp/src/main/java/com/var/OnlineShoppingApp/controller/PageController.java
+++ OnlineShoppingApp/src/main/java/com/var/OnlineShoppingApp/controller/PageController.java
@@ -3,8 +3,7 @@ package com.var.OnlineShoppingApp.controller;
 
 public class PageController {
 	public static void main(String[] args) {
-		System.out.println("hai");
-		System.out.println();
+		System.out.println("jiggelu rani");
 		
 
 		
