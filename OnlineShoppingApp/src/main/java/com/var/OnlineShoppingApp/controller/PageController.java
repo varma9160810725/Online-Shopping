c06bf334f6953a14326816c0375dddb91314b898
varma <varma.mandati@gmail.com>
2018-04-14 12:34:09 +0530

    Added  five lines code in page controller

--- OnlineShoppingApp/src/main/java/com/var/OnlineShoppingApp/controller/PageController.java
+++ OnlineShoppingApp/src/main/java/com/var/OnlineShoppingApp/controller/PageController.java
@@ -4,5 +4,12 @@ package com.var.OnlineShoppingApp.controller;
 public class PageController {
 	public static void main(String[] args) {
 		System.out.println("hai");
+		System.out.println("varma");
+		System.out.println("varma");
+		System.out.println("varma");
+		System.out.println("varma");
+		System.out.println("varma");
+		System.out.println("varma");
+		System.out.println("varma");
 	}
 }
