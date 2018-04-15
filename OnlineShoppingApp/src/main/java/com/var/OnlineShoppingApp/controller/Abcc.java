d50d780d649b7ec7531e0d82bafac0a74fa67d22
varma9160810725 <35567317+varma9160810725@users.noreply.github.com>
2018-04-15 09:45:06 +0530

    Update Abcc.java

--- OnlineShoppingApp/src/main/java/com/var/OnlineShoppingApp/controller/Abcc.java
+++ OnlineShoppingApp/src/main/java/com/var/OnlineShoppingApp/controller/Abcc.java
@@ -2,9 +2,6 @@ package com.var.OnlineShoppingApp.controller;
 
 public class Abcc {
 	public static void main(String[] args) {
-		System.out.println("hai");
-		System.out.println("hai");
-		System.out.println("hai");
 		
 	}
 
